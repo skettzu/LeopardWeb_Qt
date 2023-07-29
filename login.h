b3286aad3f8db5856f5d889450e6bf7d5899a31a
skettzu <110496340+skettzu@users.noreply.github.com>
2023-07-29 00:21:43 -0400

    Integrate SQL

---
+++ login.h
@@ -0,0 +1,28 @@
+#ifndef LOGIN_H
+#define LOGIN_H
+
+#include <QWidget>
+#include <QtSql>
+#include <QtDebug>
+#include <QFileInfo>
+
+namespace Ui {
+class Login;
+}
+
+class Login : public QWidget
+{
+    Q_OBJECT
+
+public:
+    explicit Login(QWidget *parent = nullptr);
+    ~Login();
+
+private slots:
+    void on_pushButton_login_clicked();
+
+private:
+    Ui::Login *ui;
+};
+
+#endif // LOGIN_H
